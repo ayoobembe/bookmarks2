979c4e82993d4151af2ceb34080dabb07672d173
danielobembe <ayo.obembe@gmail.com>
2014-11-20 21:11:01 +0000

    Added more features: ability to display links, and ability to have user add more links - as well as associated spec files

--- lib/server.rb
+++ lib/server.rb
@@ -1,16 +1,35 @@
 require 'sinatra/base'
 require 'data_mapper'
+require 'sinatra'
 
 # class Bookmarks2 < Sinatra::Base
 
+set :views, Proc.new{File.join(root,'..', 'views')}
+
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmarks2_#{env}")
-
 require './lib/link'
-
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
  
-#   run! if app_file == $0
+get '/' do 
+	@links = Link.all 
+	erb :index
+end
+
+post '/links' do 
+	url = params["url"]
+	title = params["title"]
+	Link.create(:url => url, :title =>title)
+	redirect to('/')
+end
+
+
+
+
+
+
+
+  # run! if app_file == $0
 # end
