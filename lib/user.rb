7bc964e9db449d1d02890382c69998840b842d3a
danielobembe <ayo.obembe@gmail.com>
2014-11-21 14:01:24 +0000

    Added feature: password confirmation on signup

--- lib/user.rb
+++ lib/user.rb
@@ -3,11 +3,16 @@ class User
 
 	include DataMapper::Resource
 
+	attr_reader :password
+	attr_accessor :password_confirmation
+	validates_confirmation_of :password 
+
 	property :id, Serial
 	property :email, String
 	property :password_digest, Text
 
 	def password=(password)  #why is there an = sign here?
+		@password = password 	 #what exactly is goin on over here?
 		self.password_digest = BCrypt::Password.create(password)
 	end
 
